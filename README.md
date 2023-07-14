3f68c5e0b2928fd6290fc600df29dbadfeca28ac
rhildred <rhildred@gmail.com>
2023-07-14 13:00:19 -0400

    use await

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # sqlite-test
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-fh6cwx)
+
+Wanted to make sure that this works on stackblitz:
+
+```bash
+npm install
+npm start
+```
+
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
-const initSqlJs = require('sql-wasm.js');
+const initSqlJs = require('sql.js');
 const filebuffer = fs.readFileSync('test.sqlite');
 
-initSqlJs().then(function (SQL) {
-  // Load the db
+async function main() {
+  const SQL = await initSqlJs();
   const db = new SQL.Database(filebuffer);
-});
+  let results = db.exec('SELECT * FROM users');
+  console.log(JSON.stringify(results));
+}
+
+main();
